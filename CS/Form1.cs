e76e401d80131ab5492d861464a6963411a8807a
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2018-05-03 18:44:18 +0300

    first commit

---
+++ CS/Form1.cs
@@ -0,0 +1,79 @@
+﻿// Developer Express Code Central Example:
+// How to customize the GridControl's print output.
+// 
+// This example demonstrates how to override the default exporting process to take
+// into account a custom drawn content provided via the
+// GridView.CustomDrawFooterCell Event
+// (ms-help://DevExpress.NETv10.1/DevExpress.WindowsForms/DevExpressXtraGridViewsGridGridView_CustomDrawFooterCelltopic.htm)
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E2667
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.Utils;
+using DevExpress.XtraGrid.Views.Base;
+using DevExpress.XtraGrid.Views.Grid.ViewInfo;
+using DevExpress.XtraEditors.ViewInfo;
+using DevExpress.XtraEditors.Drawing;
+using DevExpress.XtraEditors.Repository;
+
+namespace MyXtraGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitGrid();
+            
+        }
+
+        private void InitGrid()
+        {
+            myGridControl1.DataSource = DataHelper.CreateTable(20);
+            myGridView1.GroupCount = 1;
+            myGridView1.Columns["Rating"].GroupIndex = 0;
+            RepositoryItemRatingControl repository = new RepositoryItemRatingControl();
+            myGridView1.Columns["Rating"].ColumnEdit = repository;
+            myGridControl1.RepositoryItems.Add(repository);
+            myGridView1.CustomDrawGroupRow += gridView1_CustomDrawGroupRow;
+        }
+        void gridView1_CustomDrawGroupRow(object sender, RowObjectCustomDrawEventArgs e)
+        {
+            GridGroupRowInfo rowInfo = e.Info as GridGroupRowInfo;
+            if (rowInfo != null && rowInfo.Column.FieldName == "Rating")
+            {
+                MyGridView view = sender as MyGridView;
+                MyGridViewInfo viewInfo = view.GetViewInfo() as MyGridViewInfo;
+                int textWidth = viewInfo.CalcGroupRowTextWidth(e);
+                RatingControlViewInfo info = rowInfo.Column.ColumnEdit.CreateViewInfo() as RatingControlViewInfo;
+                RatingControlPainter painter = rowInfo.Column.ColumnEdit.CreatePainter() as RatingControlPainter;
+                info.EditValue = view.GetGroupRowValue(e.RowHandle);
+                info.CalcViewInfo(e.Graphics);
+                info.Bounds = new Rectangle(textWidth, rowInfo.DataBounds.Y, info.RatingSize.Width, rowInfo.DataBounds.Height);
+                info.CalcViewInfo(e.Graphics);
+
+                ControlGraphicsInfoArgs args = new ControlGraphicsInfoArgs(info, e.Cache, rowInfo.DataBounds);
+                e.DefaultDraw();
+                painter.Draw(args);
+                e.Handled = true;
+            }
+        }
+
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            myGridControl1.ShowPrintPreview();
+        }
+
+
+
+
+    }
+}
---
+++ CS/MyGridView/MyGridView.cs
@@ -0,0 +1,70 @@
+﻿// Developer Express Code Central Example:
+// How to customize the GridControl's print output.
+// 
+// This example demonstrates how to override the default exporting process to take
+// into account a custom drawn content provided via the
+// GridView.CustomDrawFooterCell Event
+// (ms-help://DevExpress.NETv10.1/DevExpress.WindowsForms/DevExpressXtraGridViewsGridGridView_CustomDrawFooterCelltopic.htm)
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E2667
+
+using System;
+using DevExpress.XtraGrid.Views.Base;
+using System.Drawing;
+using DevExpress.Utils;
+using DevExpress.XtraGrid.Columns;
+using DevExpress.Utils.Drawing;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.XtraGrid.Drawing;
+using DevExpress.XtraGrid.Views.Grid.Drawing;
+using DevExpress.XtraGrid.Registrator;
+using DevExpress.XtraGrid.Views.Grid.ViewInfo;
+
+namespace MyXtraGrid
+{
+    [System.ComponentModel.DesignerCategory("")]
+    public class MyGridView : GridView
+    {
+        public MyGridView() : this(null) { }
+        public MyGridView(DevExpress.XtraGrid.GridControl grid) : base(grid)    {}
+        protected override string ViewName { get { return "MyGridView"; } }
+
+        protected override DevExpress.XtraGrid.Views.Printing.BaseViewPrintInfo CreatePrintInfoInstance(DevExpress.XtraGrid.Views.Printing.PrintInfoArgs args)
+        {
+            return new MyGridViewPrintInfo(args);
+        }
+
+        internal new AppearanceObject GetLevelStyle(int level, bool groupRow)
+        {
+            return base.GetLevelStyle(level, groupRow);
+        }
+
+        public RowObjectCustomDrawEventArgs GetRowObjectCustomDrawEventArgs(GraphicsCache cache, int rowHandle, Rectangle rect, out int textWidth)
+        {
+            GridRowInfo ri = (GridRowInfo)ViewInfo.RowsInfo.FindRow(rowHandle);
+            if (ri == null)
+            {
+                textWidth = 0;
+                return null;
+            }
+            ri.DataBounds = new Rectangle(0, 0, rect.Width, rect.Height);
+            RowObjectCustomDrawEventArgs args = new RowObjectCustomDrawEventArgs(cache, rowHandle, ElPainter.GroupRow, ri, ri.Appearance);
+            textWidth = (ViewInfo as MyGridViewInfo).CalcGroupRowTextWidth(args);
+            args.Handled = true;
+
+            RaiseCustomDrawGroupRow(args);
+            return args;
+        }
+
+        GridElementsPainter elPainter = null;
+        private GridElementsPainter ElPainter
+        {
+            get
+            {
+                if (elPainter == null) elPainter = (PaintStyle as GridPaintStyle).CreateElementsPainter(this);
+                return elPainter;
+            }
+        }
+    }
+}
